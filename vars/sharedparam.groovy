22ecc63ab277b976cff5a28d603d9e131228a845
Naveen Verma <naveen.verma@opstree.com>
2020-03-08 11:19:55 +0530

    added a sharedparam file

---
+++ vars/sharedparam.groovy
@@ -0,0 +1,19 @@
+def call(body) {
+    // evaluate the body block, and collect configuration into the object
+    def pipelineParams= [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = pipelineParams
+    body()
+
+    pipeline {
+        agent any
+	stages{
+        stage('checkout'){
+            steps{
+                git branch: pipelineParams.branch, credentialsId: 'GitCredentials', url: pipelineParams.scmUrl
+            }
+
+        }
+    }
+    }
+}
